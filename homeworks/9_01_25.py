7a0c1aa1f35f84c8be1f85263703924d70c39038
GlebKurt <kurtgleb007@gmail.com>
2025-01-09 14:41:39 +0100

    Made homework

---
+++ homeworks/9_01_25.py
@@ -0,0 +1,18 @@
+from random import randint
+# 1
+# def set_table(surname, count):
+#     print(f'Столик B{randint(1, 10)} заброньовано для {surname} на {count} осіб')
+#
+# surname = input('Введіть ваше прізвище: ')
+# set_table(surname, 2)
+
+# 2
+# end_count = 0
+# while end_count != 2:
+#     def set_table(surname, count):
+#         global end_count
+#         print(f'Столик B{randint(1, 10)} заброньовано для {surname} на {count} осіб')
+#         end_count += 1
+#
+#     surname = input('Введіть ваше прізвище: ')
+#     set_table(surname, 2 if end_count == 0 else 4)
