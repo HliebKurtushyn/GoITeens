3121e6507738e341748a9d8c1174784d3007baae
GlebKurt <kurtgleb007@gmail.com>
2025-01-09 14:41:51 +0100

    Made tasks

---
+++ tasks/6_01_25.py
@@ -0,0 +1,36 @@
+# 1
+# string = input()
+# print(string[::-1])
+
+# 2
+# string = 'Hello, world!'
+# final_string = ''
+#
+# for element in string:
+#     final_string = element + final_string
+# print(final_string)
+
+# or
+
+# string = 'Hello, world!'
+# reversed_string = "".join(reversed(string))
+# print(reversed_string)
+
+# 3
+# string = '122454ав'
+#
+# for element in string:
+#     if element.isdigit():
+#         continue
+#     else:
+#         print('В рядку не лише числа')
+#         break
+
+# 4
+# string = input()
+# final_string = string.replace(' ', '')
+# print(final_string)
+
+# 5
+# string = 'hello world'
+# print(string.title())
