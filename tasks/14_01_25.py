33e6d3808ea27c70b25035df892e1b218e8ab50f
GlebKurt <kurtgleb007@gmail.com>
2025-01-14 19:32:19 +0100

    Made tasks

---
+++ tasks/14_01_25.py
@@ -0,0 +1,51 @@
+# 1
+# square = lambda x: x ** 2
+# print(square(5))
+
+# 2
+# kratne = lambda x, y: x % y == 0
+# print(kratne(240, 3))
+
+# 3
+# arithmetic = lambda x, y: (x + y) / 2
+# print(arithmetic(5, 21))
+
+# 4
+# convert_celsius = lambda x: x * 1.8 + 32
+# convert_fahrenheit = lambda x: x / 1.8 - 32
+# print(convert_celsius(20), convert_fahrenheit(0))
+
+# 5
+
+# 6
+# visokosniy = lambda x: x % 4 == 0 and x % 100 != 0 or x % 400 == 0
+# print(visokosniy(2025))
+
+# 7
+# max_num = lambda x, y, z: max(x, y, z)
+# print(max_num(5, 21, 77))
+
+# 8
+add = lambda x, y: x + y
+subtract = lambda x, y: x - y
+multiply = lambda x, y: x * y
+divide = lambda x, y: x / y
+
+keywords = {
+    'add': add,
+    'subtract': subtract,
+    'multiply': multiply,
+    'divide': divide
+}
+
+while True:
+    numbers = input("Enter two numbers separated by space:              (Just enter to exit)").split()
+    if numbers == []: break
+    do = input("Enter what to do: ")
+
+    result = keywords[do](int(numbers[0]), int(numbers[1]))
+    print(result)
+
+
+
+
