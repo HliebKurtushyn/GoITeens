fa246bc5041048f7645a9512a441ae76491a6850
GlebKurt <kurtgleb007@gmail.com>
2025-01-14 17:14:11 +0100

    Made tasks

---
+++ tasks/09_01_25.py
@@ -0,0 +1,75 @@
+# 1
+# vowels_letters = ['a', 'e', 'i', 'o', 'u', 'y']
+# def vowels(string):
+#     count = 0
+#     for letter in string:
+#         if letter in vowels_letters:
+#             count += 1
+#     return count
+#
+# string = input('Enter string: ').lower()
+# print(vowels(string))
+from enum import unique
+
+
+# 2
+# def summ(num1, num2):
+#     return num1 + num2
+#
+# number1 = int(input('Enter first number: '))
+# number2 = int(input('Enter second number: '))
+# print(summ(number1, number2))
+
+# 3
+# def max_number(numbers):
+#     return max(numbers)
+#
+#
+# numbers = input('Enter some numbers: ').split()
+# print(max_number(numbers))
+
+# 4
+# numbers = [1, 1, 2, 3, 3, 4]
+# unique_numbers = []
+#
+# def unique_elements(elements):
+#     return list(set(elements))
+#
+# print(unique_elements(numbers))
+
+# 5
+# def square_summ(numbers):
+#     return sum([int(number) ** 2 for number in numbers])
+#
+#
+# numbers_list = input('Enter some numbers: ').split()
+# print(square_summ(numbers_list))
+
+# 6
+# def revers_list(list):
+#     return list[::-1]
+#
+# elements = input('Enter some elements: ').split()
+# print(revers_list(elements))
+
+# 7
+# def bigger_than_average(numbers):
+#     count = 0
+#     middle = len(numbers) // 2
+#     for number in numbers:
+#         if number > middle:
+#             count += 1
+#     return count
+#
+# numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(bigger_than_average(numbers_list))
+
+
+
+
+
+
+
+
+
+
