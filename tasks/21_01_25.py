31c3271a01c8c5e8b67015de94da89dfcab116d6
GlebKurt <kurtgleb007@gmail.com>
2025-01-23 16:53:14 +0100

    Made tasks

---
+++ tasks/21_01_25.py
@@ -0,0 +1,47 @@
+# 1
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(numbers[0:10:2])
+
+# 2
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(numbers[1:10:2])
+
+# 3
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(numbers[2:10:3])
+
+# 4
+# pip = 'Ivan Ivanov'
+# delimiter = ' '
+# del_pos = pip.find(delimiter)
+#
+# name = pip[0:del_pos]
+# surname = pip[del_pos + 1:]
+#
+# print('First name is: ', name)
+# print('Last name is: ', surname)
+
+# 5
+# phones = ['+38(067) 999-99-99', '+38(066) 999-99-99','+38(067) 888-88-88', '+38(068) 777-77-77']
+# operator_codes = ['067', '097', '068']
+# count = 0
+#
+# for code in phones:
+#     op_code = code[4:4+3]
+#     if op_code in operator_codes:
+#         count += 1
+# print(f"Phone numbers with Kiyvstar code: {count}")
+
+# 6
+# numbers = '067', '097', '068', '050', '066', '099', '0063', '093'
+# print(set(numbers))
+
+# 7
+# numbers = {1, 2, 3}
+# numbers.add(4)
+# print(numbers)
+
+# 8
+# numbers = {1, 2, 3}
+# numbers.remove(3)
+# print(numbers)
