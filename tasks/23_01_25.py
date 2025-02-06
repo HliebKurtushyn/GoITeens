d38b4838c3a7248dc645fc0588c80e9dd4e82025
GlebKurt <kurtgleb007@gmail.com>
2025-02-06 16:43:07 +0100

    Made tasks

---
+++ tasks/23_01_25.py
@@ -0,0 +1,39 @@
+# 1
+# prime_numbers = {2, 3, 5, 7, 11, 13, 17, 19, 23}
+# if 3 in prime_numbers:
+#     print("3 is in prime numbers!")
+
+# 2
+# def same_elements(tup1, tup2):
+#     return len(set(tup1) & set(tup2))
+#
+# tuple_a = (1, 2, 3, 4, 5)
+# tuple_b = (4, 5, 6, 7, 8)
+#
+# res = same_elements(tuple_a, tuple_b)
+# print(res)
+
+# 3
+# def max_and_min(tupple):
+#     return(min(tupple), max(tupple))
+#
+# tup_1 = (1, 2, 4, 5, 7)
+# res = max_and_min(tup_1)
+# print(res)
+
+# 4
+# def reversed_tuple(tuple):
+#     return tuple[::-1]
+#
+# tup_1 = (1, 2, 3)
+# res = reversed_tuple(tup_1)
+# print(res)
+
+# 6
+# def concatenate(tuple1, tuple2):
+#     return tuple1 + tuple2
+# tuple_1 = ("Hello,")
+# tuple_2 = (" world!")
+# res = concatenate(tuple_1, tuple_2)
+#
+# print(res)
