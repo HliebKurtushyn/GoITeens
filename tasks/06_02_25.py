dd76612f81b3ee30ee8124f363c6ebd33be07f67
GlebKurt <kurtgleb007@gmail.com>
2025-02-11 16:52:10 +0100

    Made tasks

---
+++ tasks/06_02_25.py
@@ -0,0 +1,53 @@
+# 1
+# month = {
+#     "January": 1,
+#     "February": 2,
+#     "March": 3,
+#     "April": 4,
+#     "Mai":5,
+#     "June": 6,
+#     "July": 7,
+#     "August": 8,
+#     "September": 9,
+#     "October": 10,
+#     "November": 11,
+#     "December": 12
+# }
+# for m in month:
+#     print(m)
+
+# 2
+# currencies = {
+#     "USD": 40,
+#     "EUR": 45,
+#     "GBP": 50
+# }
+# print(f"USD in UAH: {currencies["USD"]}")
+
+# 3
+# fruits = {
+#     "Apple": 100,
+#     "Banana": 200,
+#     "Pear": 500
+# }
+# print(fruits["Banana"])
+
+# 4
+# people = {
+#     "Gleb": 13,
+#     "Matvii": 15,
+#     "Sasha": 14
+# }
+# ages = people.keys()
+# print(max(ages))
+
+# 5
+cities = {
+    "Київ": 2800000,
+    "Львів": 720000,
+    "Одеса": 1010000,
+    "Дніпро": 980000
+}
+
+old = cities.keys()
+print(max(old))
